d33d392519ba40159d3e436a75bcf83ad7a639ca
misskecupbung <ananda.dwirahmawati313@gmail.com>
2021-05-23 21:48:03 +0700

    edit views register & login

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,15 @@
 @extends('layouts.app')
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+</head>
+<body>
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
@@ -21,3 +31,6 @@
     </div>
 </div>
 @endsection
+</body>
+</html>
+
